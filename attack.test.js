ad22a79df761750389327ad95846817da5da4e24
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-04-13 19:24:28 +0200

    add jest globally and update ci jobs name

--- attack.test.js
+++ attack.test.js
@@ -1,9 +1,8 @@
-import { test, expect } from 'jest';
-
 const attack = require('./attack');
 
 test('should return damage sentence when called correctly', () => {
   const result = attack('Smelly Ogre', 23);
+  expect(result).toMatch(/^Smelly Ogre(.*)23/);
   expect(result).toBe('Smelly Ogre dealt 23 damage!');
   expect(result).toMatchInlineSnapshot('"Smelly Ogre dealt 23 damage!"');
 });
