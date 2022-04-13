72be2f5277d7afd5308b53d0c4ebe5d0a844fc2e
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-04-13 16:21:38 +0200

    fix linting

--- attack.js
+++ attack.js
@@ -1,3 +1,3 @@
-const attack = (creatureName, damage, isCritical) => `${creatureName} dealt ${isCritical ? damage * 2 : damage}`
+const attack = (creatureName, damage, isCritical) => `${creatureName} dealt ${isCritical ? damage * 2 : damage}`;
 
-module.exports = attack
+module.exports = attack;
