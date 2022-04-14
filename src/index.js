f71de2e00dd3f9900e3700cadcaab38650afdb10
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2022-04-14 14:29:25 +0200

    add html functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import attack from './attack';
 
-attack('Smelly Ogre', 23);
+const resultTag = document.querySelector('#resultTag');
+const creature = document.querySelector('#creature');
+const damage = document.querySelector('#damage');
+const resultButton = document.querySelector('#resultButton');
+
+resultButton.addEventListener('click', () => {
+  const result = attack(creature.value, damage.value);
+  resultTag.innerHTML = result;
+});
